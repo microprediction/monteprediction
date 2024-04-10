647b2f6ac3abb9c97e625fbce7cbb42a61f6450a
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2024-04-09 23:28:08 -0400

    Update __init__.py

--- monteprediction/__init__.py
+++ monteprediction/__init__.py
@@ -1,3 +1,4 @@
 MONTE_URL = 'https://micromonte.pythonanywhere.com/upload'
+VERIFICATION_URL = 'https://micromonte.pythonanywhere.com/verify'
 SPDR_ETFS = ['XLB', 'XLC', 'XLE', 'XLF', 'XLI', 'XLK', 'XLP', 'XLRE', 'XLU', 'XLV', 'XLY']
 KERNEL_RATE = 300.0
