9db295187306d2d9b98915424c99076d3c254323
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2024-05-11 11:41:27 -0400

    Update test_optionsutil.py

--- tests/test_optionsutil.py
+++ tests/test_optionsutil.py
@@ -1,6 +1,13 @@
 
-from monteprediction.optionsutil import get_weekly_options
+from monteprediction.optionsutil import get_weekly_options, get_weekly_implied
 
 def test_weekly_options():
     df = get_weekly_options()
     assert(len(df.index)>5)
+
+
+def test_weekly_implied():
+    d = get_weekly_implied()
+    assert 'XLB' in d
+
+
