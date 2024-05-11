204e6b6e228b31f9c6d857e06985e332ad5b6b6b
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2024-05-10 21:25:31 -0400

    Update api.py

--- monteprediction/api.py
+++ monteprediction/api.py
@@ -12,8 +12,14 @@ def convert_date(date_string):
 
 API_BASE = 'https://www.monteprediction.com/api/'
 
+def get_weekly():
+   # Get weekly scores
+   response = requests.get(API_BASE + 'weekly')
+   data = json.loads(response.json()['data'])
+   return pd.DataFrame(data)
+
 def get_implied():
-   # Get relative implied vols
+   # Get (stale) implied vols
    response = requests.get(API_BASE + 'implied')
    data = json.loads(response.json()['data'])
    return pd.DataFrame(data)
