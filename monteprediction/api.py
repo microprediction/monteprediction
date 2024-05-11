07a206a7dd86b69a9a0758043e0d48e802d570a8
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2024-05-10 21:19:05 -0400

    Update api.py

--- monteprediction/api.py
+++ monteprediction/api.py
@@ -1,11 +1,27 @@
 import requests
 import json
 import pandas as pd 
+from dateutil import parser
+
+def convert_date(date_string):
+    try:
+        dt = parser.parse(date_string)
+        return dt.strftime('%Y-%m-%d')
+    except ValueError:
+        # Handle cases where the date string is not recognized
+        return "Invalid date format"
 
 API_BASE = 'https://www.monteprediction.com/api/'
 
 def get_implied():
    # Get relative implied vols
    response = requests.get(API_BASE + 'implied')
+   data = json.loads(response.json()['data'])
+   return pd.DataFrame(data)
+
+def get_truth(expiry:str):
+   # Get "official" truth ... weekly returns
+   expiry_underscore = covert_date(expiry).replace('-','_')
+   response = requests.get(API_BASE + 'truth/' + expiry_underscore
    data = json.loads(response.json()['data'])
    return pd.DataFrame(data)
