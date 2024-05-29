8e12019c687b6a46dfbd956c97ef672bb369a380
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2024-05-29 15:43:38 -0400

    Update test_getters.py

--- tests/api/test_getters.py
+++ tests/api/test_getters.py
@@ -2,5 +2,5 @@
 
 def test_get_correlation():
    from monteprediction.api import get_correlation
-   corr = get_correlation()
+   corr = get_correlation(expiry='2024-05-13')
    assert len(corr.columns)==11
